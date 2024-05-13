12983b951a6520425ce9e7cdfbcf3ce67fd4d70f
SnagaiS <s.nagai.cingroup2012@gmail.com>
2024-05-13 18:04:01 +0900

    data-typeの実装

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,11 @@
+package basicSyntax;
+
+//提出はありませんでしたが前項作成のクラスを残しています
+public class Main {
+
+	public static void main(String[] args) {
+		// このようにコメントを残すことができます。
+		System.out.println("Hello Geekation");
+	}
+
+}
