39700bb058fd1d2bc362798f5970505b97fc9139
SnagaiS <s.nagai.cingroup2012@gmail.com>
2024-05-24 01:29:01 +0900

    クラス

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,13 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 53;
+		double height = 122.22;
+		GeekIntroduction.greeting(age, height);
+
+		GeekIntroduction.specialSkill(1, 20);
+	}
+
+}
